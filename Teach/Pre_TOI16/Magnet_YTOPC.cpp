8d34160fd6faad9d305d2662b0f2fe6c62e2aebc
Phumipat C <phumipat.c@outlook.com>
2020-11-22 12:13:24 +0700

    Pre TOI16

---
+++ Teach/Pre_TOI16/Magnet_YTOPC.cpp
@@ -0,0 +1,35 @@
+/*
+	Author	: Phumipat C. [MAGCARI]
+	School	: RYW
+	Language: C++
+	Algo	: 
+	Status	: 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int ,int > PII;
+typedef pair<long long ,long long > PLL;
+map<int ,int > mapp;
+vector<int > e;
+int main(){
+	ios_base::sync_with_stdio(0);	cin.tie(0),cout.tie(0);
+	int n,m,q,u,w,idx;
+	cin >> n >> m >> q;
+	while(m--){
+		cin >> u >> w;
+		mapp[u]++;
+		mapp[u+w]++;
+	}
+	e.push_back(1);
+	for(auto x:mapp)
+		if(x.second%2)
+			e.push_back(x.first);
+	e.push_back(n+1);
+	while(q--){
+		cin >> u;
+		idx = upper_bound(e.begin(),e.end(),u)-e.begin();
+		cout << e[idx]-e[idx-1] << '\n';
+	}
+	return 0;
+}
---
+++ Teach/Pre_TOI16/RT_Hire_friends.cpp
@@ -0,0 +1,31 @@
+/*
+	Author	: Phumipat C. [MAGCARI]
+	School	: RYW
+	Language: C++
+	Algo	: 
+	Status	: 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int ,int > PII;
+typedef pair<long long ,long long > PLL;
+vector<PII > e;
+int main(){
+	ios_base::sync_with_stdio(0);	cin.tie(0),cout.tie(0);
+	int n,u,v,sum = 0;
+	LL ans = 0;
+	cin >> n;
+	while(n--){
+		cin >> u >> v;
+		e.push_back({u,1});
+		e.push_back({v+1,-1});
+	}
+	sort(e.begin(),e.end());
+	for(int i=0;i<e.size()-1;i++){
+		sum+=e[i].second;
+		ans+=(LL )(e[i+1].first-e[i].first)*max(0,sum-1);
+	}
+	cout << ans << '\n';
+	return 0;
+}
