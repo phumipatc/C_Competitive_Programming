ecc02b1b1457f147e175c57d7efc068252801a15
Phumipat C <phumipat.c@outlook.com>
2020-12-10 14:35:50 +0700

    KU01 Online (3rd round)

--- Website/KU01/Drones.cpp
+++ Website/KU01/Drones.cpp
@@ -2,8 +2,8 @@
 	Author	: Phumipat C. [MAGCARI]
 	School	: RYW
 	Language: C++
-	Algo	: DP
-	Status	: Unfinished (30 pt.)
+	Algo	: Heap
+	Status	: Finished
 */
 #include<bits/stdc++.h>
 #define all(x) (x).begin(),(x).end()
@@ -26,33 +26,25 @@ LL maxN(vector<LL > a){
 		maxx = max(maxx,x);
 	return maxx;
 }
-vector<int > a;
-int qs[1010],dp[1010][210];
+int a[1010],countt[210];
+priority_queue<PII > heap;
 int main(){
 	ios_base::sync_with_stdio(0);	cin.tie(0),cout.tie(0);
-	int n,m = 200;
+	int n,ans = 0;
 	cin >> n;
-	a.resize(n+1);
 	for(int i=1;i<=n;i++)
 		cin >> a[i];
-	sort(allst(a,1));	reverse(allst(a,1));
-	for(int i=1;i<=n;i++)
-		qs[i] = a[i]+qs[i-1];
-	for(int i=0;i<=n;i++)
-		for(int j=0;j<=200;j++)
-			dp[i][j] = 1e9;
-	dp[0][0] = 0;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=m;j++){
-			for(int k=i-1;k>=i-10 && k>=0;k--){
-				dp[i][j] = min(dp[i][j],dp[k][j-1] + (qs[i]-qs[k])*j*2 - (qs[k+1]-qs[k])*j);
-			}
-			// printf("%d %d %d\n",i,j,dp[i][j]);
-		}
+	sort(a+1,a+n+1);
+	for(int i=1;i<=200;i++)
+		heap.push({-i,i});
+	for(int i=n;i>=1;i--){
+		auto now = heap.top();
+		heap.pop();
+		// printf("%d\n",-now.first);
+		ans-=now.first*a[i];
+		countt[now.second]++;
+		if(countt[now.second]!=10)	heap.push({-now.second*2,now.second});
 	}
-	int minn = 1e9;
-	for(int i=1;i<=m;i++)
-		minn = min(minn,dp[n][i]);
-	cout << minn << '\n';
+	cout << ans << '\n';
 	return 0;
 }
