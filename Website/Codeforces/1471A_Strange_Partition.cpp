d3ebeeff81f233df5ff9a8e98742ab5224090c47
Phumipat C <phumipat.c@outlook.com>
2021-01-07 23:16:46 +0700

    Codeforces 1471

---
+++ Website/Codeforces/1471A_Strange_Partition.cpp
@@ -0,0 +1,59 @@
+/*
+	Author	: Phumipat C. [MAGCARI]
+	School	: RYW
+	Language: C++
+	Algo	: Greedy, Math
+	Status	: Finished
+*/
+#include<bits/stdc++.h>
+#define all(x) (x).begin(),(x).end()
+#define allst(x,y) (x).begin()+y,(x).end()
+#define MOD (LL )(1e9+7)
+using namespace std;
+using LL = long long;
+using PII = pair<int ,int >;
+using PLL = pair<long long ,long long >;
+const int dir4[2][4] = {{1,-1,0,0},{0,0,1,-1}};
+const int dir8[2][8] = {{-1,-1,-1,0,1,1,1,0},{-1,0,1,1,-1,0,1,-1}};
+const int MAX_INT = 1e9;
+const LL MAX_LL = 1e18;
+LL minN(vector<LL > a){
+	LL minn = 1e18;
+	for(auto x:a)
+		minn = min(minn,x);
+	return minn;
+}
+LL maxN(vector<LL > a){
+	LL maxx = -1e18;
+	for(auto x:a)
+		maxx = max(maxx,x);
+	return maxx;
+}
+LL modN(LL a,LL b,LL c = MOD){
+	if(b == 0)	return 0;
+	if(b == 1)	return a%c;
+	LL now = modN(a,b/2);
+	if(b&1)	return (((now*now)%c)*(a%c))%c;
+	else	return (now*now)%c;
+}
+void solve(){
+	int n,x,num;
+	LL maxx = 0,minn = 0;
+	cin >> n >> x;
+	for(int i=1;i<=n;i++){
+		cin >> num;
+		maxx+=ceil((double )num/x);
+		minn+=num;
+	}
+	minn = ceil((double )minn/x);
+	cout << minn << ' ' << maxx << '\n';
+}
+int main(){
+	ios_base::sync_with_stdio(0);	cin.tie(0),cout.tie(0);
+	int q = 1;
+	cin >> q;
+	while(q--){
+		solve();
+	}
+	return 0;
+}
