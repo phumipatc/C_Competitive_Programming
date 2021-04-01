514f84ee390b76e6437399a0cd6cc5a5b644063b
Phumipat C <phumipat.c@outlook.com>
2021-04-01 21:34:33 +0700

    Codeforces Div.3 [1506]

---
+++ Website/Codeforces/1506/1506B_Partial_Replacement.cpp
@@ -0,0 +1,75 @@
+/*
+	Author	: Phumipat C. [MAGCARI]
+	School	: RYW
+	Language: C++
+	Algo	:
+	Status	:
+*/
+#include<bits/stdc++.h>
+#define all(x) (x).begin(),(x).end()
+#define allst(x,y) (x).begin()+y,(x).end()
+#define decp(x) fixed << setprecision(x)
+#define MOD (LL )(1e9+7)
+using namespace std;
+using LL = long long;
+using PII = pair<int ,int >;
+using PLL = pair<long long ,long long >;
+const int dir4[2][4] = {{1,-1,0,0},{0,0,1,-1}};
+const int dir8[2][8] = {{-1,-1,-1,0,1,1,1,0},{-1,0,1,1,-1,0,1,-1}};
+const int MAX_INT = 1e9;
+const LL MAX_LL = 1e18;
+LL minN(vector<LL > a){
+	LL minn = 1e18;
+	for(auto x:a)
+		minn = min(minn,x);
+	return minn;
+}
+LL maxN(vector<LL > a){
+	LL maxx = -1e18;
+	for(auto x:a)
+		maxx = max(maxx,x);
+	return maxx;
+}
+LL modN(LL a,LL b,LL c = MOD){
+	if(b == 0)	return 0;
+	if(b == 1)	return a%c;
+	LL now = modN(a,b/2);
+	if(b&1)	return (((now*now)%c)*(a%c))%c;
+	else	return (now*now)%c;
+}
+int dp[55];
+void solve(){
+	int n,k,idx;
+	string a;
+	cin >> n >> k >> a;
+	for(int i=0;i<n;i++)
+		dp[i] = 1e9;
+	for(int i=0;i<n;i++){
+		if(a[i] != '*')	continue;
+		dp[i] = 1;
+		break;
+	}
+	for(int i=n-1;i>=0;i--){
+		if(a[i] != '*')	continue;
+		idx = i;
+		break;
+	}
+	for(int i=0;i<=idx;i++){
+		if(a[i] != '*')	continue;
+		for(int j=i-1;j>=i-k && j>=0;j--){
+			if(a[j] != '*')	continue;
+			dp[i] = min(dp[i],dp[j]+1);
+		}
+	}
+	cout << dp[idx] << '\n';
+}
+int main(){
+	ios_base::sync_with_stdio(0);	cin.tie(0),cout.tie(0);
+	int q = 1;
+	cin >> q;
+	for(int Q=1;Q<=q;Q++){
+		// cout << "Case #" << Q << ": ";
+		solve();
+	}
+	return 0;
+}
