e1f672500119124d8b59d8e9f6bf7782c7b60b01
Phumipat C <phumipat.c@outlook.com>
2021-05-09 21:47:52 +0700

    Add "Algo" and "Status"

--- Teach/Jump_Land.cpp
+++ Teach/Jump_Land.cpp
@@ -4,8 +4,8 @@
 	School	: RYW
 	Language: C++
 	Created	: 09 May 2021 [15:42]
-	Algo	:
-	Status	:
+	Algo	: DSU
+	Status	: Finished
 */
 #include<bits/stdc++.h>
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
--- Teach/PT_Mean_Max.cpp
+++ Teach/PT_Mean_Max.cpp
@@ -4,8 +4,8 @@
 	School	: RYW
 	Language: C++
 	Created	: 09 May 2021 [19:12]
-	Algo	:
-	Status	:
+	Algo	: BS
+	Status	: Finished
 */
 #include<bits/stdc++.h>
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
