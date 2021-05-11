2fb1fccf5e413ae85b7810e7566cd56fc35097d6
Phumipat C <phumipat.c@outlook.com>
2021-05-11 08:10:54 +0700

    Exercise

---
+++ Teach/RT_Olympics.cpp
@@ -0,0 +1,58 @@
+/*
+	Task	: RT_Olympics
+	Author	: Phumipat C. [MAGCARI]
+	Language: C++
+	Created	: 11 May 2021 [08:03]
+	Algo	: 
+	Status	: 
+*/
+#include<bits/stdc++.h>
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x),end(x)
+#define allst(x,y) (x).begin()+y,(x).end()
+#define remdup(x) (x).resize(unique((x).begin(),(x).end())-(x).begin())
+#define sz(x) (int)(x).size()
+#define decp(x) fixed << setprecision(x)
+#define MOD (LL )(1e9+7)
+using namespace std;
+using LL = long long;
+using PII = pair<int ,int >;
+using PLL = pair<long long ,long long >;
+const int dir4[2][4] = {{1,-1,0,0},{0,0,1,-1}};
+const int dir8[2][8] = {{-1,-1,-1,0,1,1,1,0},{-1,0,1,1,-1,0,1,-1}};
+const int MAX_INT = 2e9;
+const LL MAX_LL = 1e18;
+LL modN(LL a,LL b,LL c = MOD){
+	if(b == 0)	return 1;
+	if(b == 1)	return a%c;
+	LL now = modN(a,b/2,c);
+	if(b&1)	return (((now*now)%c)*(a%c))%c;
+	else	return (now*now)%c;
+}
+void solve(){
+	int n,m,p;
+	cin >> n >> m >> p;
+	if(n == m){
+		cout << "yes\n";
+		return ;
+	}
+	n-=m;
+	for(int i=p;i<=m;i+=p)
+		n-=p;
+	if(n<0){
+		cout << "yes\n";
+		return ;
+	}
+	cout << "no\n";
+}
+int main(){
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(cin.failbit);
+	int q = 1;
+	cin >> q;
+	for(int Q=1;Q<=q;Q++){
+		// cout << "Case #" << Q << ": ";
+		solve();
+	}
+	return 0;
+}
