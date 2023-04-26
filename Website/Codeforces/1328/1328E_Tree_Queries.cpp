bfd81629ab07da4c863049e6d826aaa13e2e40be
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2023-04-26 16:17:25 +0700

    remove smth

--- Website/Codeforces/1328/1328E_Tree_Queries.cpp
+++ Website/Codeforces/1328/1328E_Tree_Queries.cpp
@@ -1,8 +1,8 @@
 /*
-	Task	: Long_Jump
+	Task	: 1328E_Tree_Queries
 	Author	: Phumipat C. [MAGCARI]
 	Language: C++
-	Created	: 20 November 2021 [19:45]
+	Created	: 04 April 2023 [12:54]
 	Algo	: 
 	Status	: 
 */
@@ -30,75 +30,71 @@ LL modN(LL a,LL b,LL c = MOD){
 	if(b&1)	return (((now*now)%c)*(a%c))%c;
 	else	return (now*now)%c;
 }
-const int N = 1e5+5;
+const int N = 200010;
 vector<int > g[N];
-int cnt1[N],cnt2[N];
-struct A{
-	int u,last;
-	string st;
-};
+int lv[N],lca[N][20];
+void dfs(int u,int p,int lvl = 1){
+	lv[u] = lvl;
+	lca[u][0] = p;
+	rep(i,1,18)
+		lca[u][i] = lca[lca[u][i-1]][i-1];
+	for(auto x:g[u]){
+		if(x == p)	continue;
+		dfs(x,u,lvl+1);
+	}
+}
+int findLCA(int u,int v){
+	// make sure that lv[u] is not less than lv[v];
+	if(lv[u] < lv[v])	swap(u,v);
+	repr(i,18,0){
+		if(lv[lca[u][i]] < lv[v])	continue;
+		u = lca[u][i];
+	}
+	// now lv[u] is equal to lv[v];
+	if(u == v)	return u;
+	repr(i,18,0){
+		if(lv[lca[u][i]] == lv[lca[v][i]])	continue;
+		u = lca[u][i],v = lca[v][i];
+	}
+	return lca[u][0];
+}
 void init(){
-	
+
 }
 void solve(){
-	int n,st,en,u,v;
-	string val;
-	cin >> n >> st >> en;
+	int n,m;
+	cin >> n >> m;
 	rep(i,1,n-1){
+		int u,v;
 		cin >> u >> v;
-		g[u].emplace_back(v);
-		g[v].emplace_back(u);
+		g[u].push_back(v);
+		g[v].push_back(u);
 	}
-	cin >> val;
-	val.insert(val.begin(),'0');
-
-	fill(cnt1,cnt1+N,-1);
-	fill(cnt2,cnt2+N,-1);
-	queue<A > que;
-	que.push({st,0,string()});
-	while(!que.empty()){
-		A now = que.front();
-		que.pop();
-		if(now.st.empty()){
-			if(val[now.u] == '(')	now.st.append("(");
-			else					continue;
-		}else{
-			if(val[now.u] == ')')	now.st.pop_back();
-			else					now.st.append("(");
+	dfs(1,0);
+	while(m--){
+		int num,target,maxlv = 0;
+		cin >> num;
+		vector<int > v(num);
+		for(auto &x:v){
+			cin >> x;
+			if(maxlv < lv[x]){
+				maxlv = lv[x];
+				target = x;
+			}
 		}
-		cnt1[now.u] = now.st.size();
-		for(auto x:g[now.u]){
-			if(x == now.last)	continue;
-			que.push({x,now.u,now.st});
+		for(auto &x:v){
+			if(x == target || x == 1)	continue;
+			x = lca[x][0];
 		}
+		bool yes = true;
+		for(auto x:v){
+			if(findLCA(x,target) != x){
+				yes = false;
+				break;
+			}
+		}
+		cout << (yes?"YES\n":"NO\n");
 	}
-
-	que.push({en,0,string()});
-	while(!que.empty()){
-		A now = que.front();
-		que.pop();
-		if(now.st.empty()){
-			if(val[now.u] == ')')	now.st.append(")");
-			else					continue;
-		}else{
-			if(val[now.u] == '(')	now.st.pop_back();
-			else					now.st.append(")");
-		}
-		cnt2[now.u] = now.st.size();
-		for(auto x:g[now.u]){
-			if(x == now.last)	continue;
-			que.push({x,now.u,now.st});
-		}
-	}
-	map<int ,int > mapp;
-	rep(i,1,n)
-		mapp[cnt2[i]]++;
-	LL ans = 0;
-	rep(i,1,n){
-		if(cnt1[i] == -1)	continue;
-		ans+=mapp[cnt1[i]];
-	}
-	cout << ans << '\n';
 }
 int main(){
 	cin.tie(0)->sync_with_stdio(0);
